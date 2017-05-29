51cd5dc4538e5b90f0606db7a408c5e564c1fbf1
RachmanForniandi <rachmanforniandi@gmail.com>
2017-05-29 19:08:24 +0700

    tipe data string

---
+++ basic_php/intro2.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>Belajar PHP</title>
+    </head>
+
+    <body>
+    <?php
+        /*
+            berapa baris
+            komentar
+            terserah kita2
+        */ 
+        // variabel pada php
+        //tipe data -string-
+        $nama = "sekolah koding 123";
+        $nama2 = "Bermain koding";
+
+        
+        echo $nama ." untuk ". $nama2;
+		echo "<br>Selamat datang di \$nama";
+		
+    ?>
+    
+    </body>
+</html>
