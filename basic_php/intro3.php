e81d20515212383990dfe039905022169170f648
RachmanForniandi <rachmanforniandi@gmail.com>
2017-05-29 19:47:32 +0700

    tipe data integer dan float

---
+++ basic_php/intro3.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>Belajar PHP</title>
+    </head>
+
+    <body>
+    <?php
+        /*
+            berapa baris
+            komentar
+            terserah kita2
+        */ 
+        // variabel pada php
+        //tipe data -string-
+        $nama = "sekolah koding";
+        $nama2 = "Bermain koding";
+		
+		echo "<br>Selamat datang di $nama <br>";
+		
+		//tipe data angka/number
+		// 1. Integer 2. Float
+		
+		$angka1 = 1000;
+		$angka2 = 1500;
+		$angka3 = 100.123; //tipe float
+	
+		echo $angka1 . $angka2;	
+
+		echo $angka1 + $angka3;	
+    ?>
+    
+    </body>
+</html>
