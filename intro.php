3095398f6db8579a77e2e6d9f17df65ca145d806
RachmanForniandi <rachmanforniandi@gmail.com>
2017-05-29 17:44:31 +0700

    dasar php,echo,mixing html & php

---
+++ intro.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Belajar PHP</title>
+</head>
+
+<body>
+<?php 
+	echo "<h1>Sekolah Koding</h1>";
+	echo "halo<br>";
+	echo "Para member SekolahKoding.<br>";
+?>
+
+Halo semuanya !!
+</body>
+</html>
