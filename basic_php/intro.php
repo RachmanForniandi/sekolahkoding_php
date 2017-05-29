8e1c662280b15558d9e764402e5b0059d3d5b778
RachmanForniandi <rachmanforniandi@gmail.com>
2017-05-29 18:47:07 +0700

    syntax komentar dan variabel

---
+++ basic_php/intro.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>Belajar PHP</title>
+    </head>
+
+    <body>
+    <?php
+        /*
+            berapa baris
+            komentar
+            terserah kita2
+        */ 
+        // variabel pada php
+        
+        $nama = "sekolah koding 123";
+        $_123 = "text_baru";
+        
+        echo "<h1>$_123</h1>";
+        echo "halo ".$nama."<br>";
+        echo "Member";
+    ?>
+    
+    </body>
+</html>
