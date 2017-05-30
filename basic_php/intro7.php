4d09322ecc96a110d457181882d1bb0ec3cad094
RachmanForniandi <rachmanforniandi@gmail.com>
2017-05-30 16:31:18 +0700

    pengenalan array

---
+++ basic_php/intro7.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>Belajar PHP</title>
+    </head>
+
+    <body>
+    
+    <h1>Array PHP</h1>
+    
+    <?php
+        //-------tipe data array-------
+		$kotak= array('anjing','kurakura','koala');
+		$nama= ['Hilman','Endy','Tiqa'];
+		
+		print_r($kotak);	
+		echo $nama[0];
+	?>
+    
+    </body>
+</html>
