7c92d474cd57b2ce2386aca78ea7a34b12a29d4b
RachmanForniandi <rachmanforniandi@gmail.com>
2017-06-01 17:01:18 +0700

    menampilkan error

---
+++ basic_php/intro30.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+	function halo(){
+		echo "test";
+	}
+	
+	halos();
+?>
+
+<?php echo 'halo'?>
+<? echo 'halo';?>
+<?= 'halo123'?>
+
+</body>
+</html>
