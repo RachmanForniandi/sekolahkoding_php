9097d0f4fec30ab1c1778dadb5f1db8cd35ba50d
RachmanForniandi <rachmanforniandi@gmail.com>
2017-06-01 17:13:41 +0700

    die & var_dump

---
+++ basic_php/intro31.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Belajar PHP</title>
+</head>
+
+<body>
+	<h1>Belajar PHP</h1>
+    <?php 
+	//echo print
+	//print_r dan var_dump
+	$vehicle = ['mobil', 'motor'];
+	var_dump($vehicle);
+	//die($vehicle); //utk menghentikan fungsi yg berjalan
+	
+	echo "satu dua tiga";
+	?> 
+</body>
+</html>
