2e6ccc0861df84fd121106fe948315e516c27e51
RachmanForniandi <rachmanforniandi@gmail.com>
2017-06-01 14:39:29 +0700

    fungsi pd php

---
+++ basic_php/intro24.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Belajar PHP</title>
+    </head>
+
+<body>
+	<h1>Fungsi PHP</h1>
+    <?php
+    // fungsi pada PHP 
+    
+    function panggil(){
+		echo "-----------------";
+		echo "sekolah koding";
+		echo "-----------------";
+	}
+	
+	function jarak(){
+		echo "<br>";
+	}
+	
+	panggil();
+	jarak();
+	panggil();
+	jarak();
+	panggil();
+	jarak();
+	
+	?>
+</body>
+</html>
