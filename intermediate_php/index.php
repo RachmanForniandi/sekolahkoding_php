ac9188b14d7451d8c6da0e03afcf30414b942416
RachmanForniandi <rachmanforniandi@gmail.com>
2017-06-04 19:13:53 +0700

    implode dan explode

--- intermediate_php/index.php
+++ intermediate_php/index.php
@@ -2,6 +2,20 @@
     
     <main>
     	Selamat datang di sekolah koding, tempat belajar programming online.
+        
+        <h2>Implode dan Explode</h2>
+        
+        <?php 
+		//implode (separator, array)
+		$pekerjaan= ["Front-End","Back-End","Android Developer","Graphic Designer"];
+		echo implode(" | ", $pekerjaan);
+		
+		echo "<br>";
+		
+		//explode
+		$pelajaran = "html css javascript php java";
+		print_r(explode(" ", $pelajaran));
+		?>
     </main>
     
 <?php include_once('footer.php'); ?>
