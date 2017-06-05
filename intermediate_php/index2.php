8fec7532c4bcdfc37bdc5d98215ca9c9c5f467bd
RachmanForniandi <rachmanforniandi@gmail.com>
2017-06-05 12:58:47 +0700

    Metode GET

---
+++ intermediate_php/index2.php
@@ -0,0 +1,13 @@
+<?php 
+
+//--------GET and POST-----------
+echo $_GET['nama'];
+?>
+
+<form action="index2.php" method="get">
+
+	<input type="text" name="nama">
+    <input type="password" name="password">
+    <input type="submit" name="submit">
+
+</form>
