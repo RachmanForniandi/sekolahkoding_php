c9a8ea267c4ea5c9ae4b5b47587847054093d421
RachmanForniandi <rachmanforniandi@gmail.com>
2017-06-14 15:43:36 +0700

    mengingat data dgn cookie

--- intermediate_php/index2.php
+++ intermediate_php/index2.php
@@ -10,18 +10,22 @@ $password = 'forniandi123';
 
 if(isset($_POST['submit'])){
 	
-	if($_POST['nama'] == $user &&
+	if($_POST['nama'] == $username &&
 	   $_POST['password']== $password){
 		   
+		//utk cookie
+		//setcookie(key, nilai, expire)
+		setcookie('nama_user', $_POST['nama'], time()+180);
+		   
 		//memindahkan ke halaman profile
-		header('Location: profile.php' . $username);	
+		header('Location: profile.php');	
 	}else{
 		echo 'Login gagal. Silahkan coba lagi. ';
 		}
 	}
 ?>
 
-<form action="profile.php" method="get">
+<form action="index2.php" method="post">
 
 	<input type="text" name="nama">
     <input type="password" name="password">
--- intermediate_php/profile.php
+++ intermediate_php/profile.php
@@ -1,3 +1,6 @@
-<?php 
-	echo 'ini halaman profile ' .$_GET['nama'];
+<?php
+	//cookie expired 
+	//setcookie('nama_user', $_POST['nama'], time()-180);
+	
+	echo 'ini halaman profile ' . $_COOKIE['nama_user'];
 ?>
