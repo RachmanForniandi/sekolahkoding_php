b11c04ea647e2819ca3a778f7f9c5e9d01209c2c
RachmanForniandi <rachmanforniandi@gmail.com>
2017-06-21 16:38:07 +0700

    validasi upload gambar/data

--- intermediate_php/index3.php
+++ intermediate_php/index3.php
@@ -3,12 +3,36 @@
 if(isset($_POST['submit'])){
 	echo $_FILES['gambar']['name'];
 	
-	$nama = $_FILES['gambar']['name'];
+	$nama   = $_FILES['gambar']['name'];
+	$error  = $_FILES['gambar']['error'];
+	$size   = $_FILES['gambar']['size'];
 	$lokasi = $_FILES['gambar']['tmp_name'];
+	$format = $_FILES['gambar']['type'];
 	
+	//$ex = pathinfo($nama, PATHINFO_EXTENSION);
+	//die($ex);
+	
+	if($error == 0){
+		if($size < 1000000){
+	
+	if($format == 'image/jpeg' || 'images/png'){
+		//utk upload
 	move_uploaded_file($lokasi, 'upload/'. $nama);
+	echo 'upload berhasil!';		
+	
+		}else{
+			echo 'formatnya harus jpeg atau png bro';
+		}
+	
+		}else{
+			echo 'size itemnya kegedean coy!';
+		}
+		
+		}else{
+			echo 'upload error !';
+	}
+		
 }
-
 
 ?>
 
